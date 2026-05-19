705777c2d298eecf7e84a53c0cb6afec2808e11d
Pooya Parsa <pooya@pi0.io>
2026-05-19 09:10:26 -0300

    fix(vite): pass correct `root` config in builder

--- src/build/vite/build.ts
+++ src/build/vite/build.ts
@@ -6,11 +6,14 @@ export async function viteBuild(nitro: Nitro) {
   if (nitro.options.dev) {
     throw new Error("Nitro dev CLI does not supports vite. Please use `vite dev` instead.");
   }
-  const { createBuilder } = await import((nitro.options as any).__vitePkg__ || "vite");
+  const { createBuilder } = (await import(
+    (nitro.options as any).__vitePkg__ || "vite"
+  )) as typeof import("vite");
+
   const pluginInstance = nitroPlugin({ _nitro: nitro });
   (globalThis as any).__nitro_build__ = true;
   const builder = await createBuilder({
-    base: nitro.options.rootDir,
+    root: nitro.options.rootDir,
     plugins: [pluginInstance],
     logLevel: isTest ? "warn" : undefined,
   });
